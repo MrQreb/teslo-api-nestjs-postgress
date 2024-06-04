0f5a5553386d6386dd5f30b57799cd79eed81069
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-04 17:29:22 -0600

    valition pipes added and prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  //prefix para habilitar la ruta de la API
+  app.setGlobalPrefix('api');
+  
+  //Validacion de Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //whitelist => permite que solo los campos definidos en la clase DTO sean válidos
+      forbidNonWhitelisted: true, //forbidNonWhitelisted => evita que se envíen campos que no están definidos en la clase DTO
+      transform: true, //transform => convierte los tipos de datos a los tipos definidos en la clase DTO
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();

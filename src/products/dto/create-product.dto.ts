adf498d6e9105e6ab2927cc9b22c9cd68dc1d8c4
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-04 17:57:08 -0600

    CreateProductDto finished

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,36 @@
-export class CreateProductDto {}
+import { IsInt, IsArray , IsNumber, IsOptional,
+         IsPositive, IsString, MinLength, IsIn } from "class-validator";
+
+export class CreateProductDto {
+   
+   
+    @IsString()
+    @MinLength(3)
+    title:string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?:number;
+
+    @IsString()
+    @IsOptional()
+    description?:string;
+
+    @IsString()
+    @IsOptional()
+    slug?:string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    strock?:number;
+
+
+    @IsString({each:true}) //each:true => valida cada elemento del array
+    @IsArray()
+    sizes:string[];
+
+    @IsIn(['men','kid','women','unisex'])
+    gender:string;
+}

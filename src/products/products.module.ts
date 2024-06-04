f613ba78e778df4f5a0607c26850cb09eb306f7c
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-04 17:04:23 -0600

    using TypeOrmModule and importing Produc Entitiy

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+import { Product } from './entities/product.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
+  imports: [
+    
+    //TypeOrmModule => permite la integración de TypeORM con NestJS
+    //forFeature => permite definir las entidades que se van a utilizar en el módulo
+    // [Product,Clothe] => se definen las entidades que se van a utilizar en el módulo
+    TypeOrmModule.forFeature([Product]	),
+  ]
 })
 export class ProductsModule {}

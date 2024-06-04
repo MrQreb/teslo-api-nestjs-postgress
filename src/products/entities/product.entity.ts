ff64603de1f1bdac02160f24cbd40622666bc3bd
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-04 16:42:42 -0600

    table products created

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,17 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+//Entity => define una clase como una entidad de la base de datos
+@Entity()
+export class Product {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+
+    @Column('text',{
+
+        //Constraints => permite definir restricciones en la base de datos
+        unique: true
+    })
+    title: string;
+}

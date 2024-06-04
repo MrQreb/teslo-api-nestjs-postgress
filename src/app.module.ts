c7a7be1d37ca8a1c71dbcd872f79894113c1f753
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-04 13:10:33 -0600

    app.module configured to use bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 
 @Module({
-  imports: [],
+  imports: [
+
+    // Permite usar variables de entorno
+    ConfigModule.forRoot(),
+
+    //Configurar typeOrm
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      
+      
+      autoLoadEntities: true, // Carga las entidades de forma automática
+      synchronize: true, // Crea las tablas automáticamente 
+      //syncronize: false, en producción
+    }),
+  ],
   controllers: [],
   providers: [],
 })

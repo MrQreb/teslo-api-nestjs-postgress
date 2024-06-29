fa7d84f99cfdb500c42a9dd5b3a2e75879a411a9
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-29 13:46:03 -0600

    files.service method getStaticProuctImage finished

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 
 
 @Injectable()
 export class FilesService {
   
+    getStaticProductImage(imageName: string) {
+
+        const path = join(__dirname, '../../static/products', imageName); // se une la ruta del archivo con el nombre del archivo
+
+        if( !existsSync(path) )
+             throw new BadRequestException( `no product found with image ${imageName}` ); 
+
+        return path; // se retorna la ruta del archivo
+
+    }
 }

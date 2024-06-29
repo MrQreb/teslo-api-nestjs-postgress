acc6e28bb366597d51bc33512d5c10d2f7f79979
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-06-29 13:47:21 -0600

    files.module ConfigModule imported to use env

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { AppModule } from 'src/app.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [FilesController],
   providers: [FilesService],
+  imports:[
+    ConfigModule // exportar el modulo de configuración para el path en files
+  ]
 })
 export class FilesModule {}

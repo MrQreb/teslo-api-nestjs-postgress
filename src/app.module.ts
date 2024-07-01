e83e90b16e59c79d92a68a6fa186c461b5bc5704
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-07-01 15:50:34 -0600

    app.module 'ServeStaticModule' configured to save static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
@@ -28,6 +32,13 @@ import { FilesModule } from './files/files.module';
       synchronize: true, // Crea las tablas automáticamente 
       //syncronize: false, en producción
     }),
+
+    //Configurar el servidor estático para servir archivos
+    ServeStaticModule.forRoot({
+    rootPath: join(__dirname,'..','public'),
+    }) ,
+
+    
 
     ProductsModule,
 

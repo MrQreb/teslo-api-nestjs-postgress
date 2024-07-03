03962c10e6419a24c112130c2f0f44c172b849d6
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-07-03 17:07:32 -0600

    user.entity triggers added

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('users')
 export class User {
@@ -30,6 +30,18 @@ export class User {
         default:['user']
     })
     roles:string[];
+
+    //Trigers
+    @BeforeInsert()
+    checkFieldsBeforeInsert(){
+        this.email = this.email.toLowerCase().trim();
+    }
+
+    @BeforeUpdate()
+    checkFieldsBeforeUpdate(){
+        this.email = this.email.toLowerCase().trim();
+    }
+
 
 
 

63e103bee2c1c2b526b18a44ba5b68b0fbeb7985
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2024-07-06 15:04:34 -0600

    auth decorator barrel file updated

--- src/auth/decorators/index.ts
+++ src/auth/decorators/index.ts
@@ -1,3 +1,8 @@
+
 export { GetUser } from "./get-user.decorator";
 export { RawHeaders } from "./raw-headers.decorator";
+
+export { Auth } from "./auth.decorator";
+export { RoleProtected } from "./role-protected.decorator";
+
 
